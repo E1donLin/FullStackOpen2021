944f60aef51b5fcc1678a46927ca2e53b62e27e5
YuJun <yl4039@rit.edu>
2021-06-13 14:37:04 -0400

    factered out utils

---
+++ part4/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((err) => {
+    logger.error('error connection to MongoDB', err.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,25 +1,10 @@
+const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogRouter = require('./controllers/blogs')
-require('dotenv').config()
+const app = require('./app')
+const logger = require('./utils/logger')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-})
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
-
-const PORT = 3003
+const PORT = config.PORT
 const server = http.createServer(app)
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
